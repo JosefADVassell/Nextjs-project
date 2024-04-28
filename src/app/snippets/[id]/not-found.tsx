888fdafd5c1bc0878dbb928dafc865bcd9293964
Josef Vassell <Josefvassell@outlook.com>
2024-04-27 23:45:37 -0500

    updates to the project

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function SnippetNotFound() {
+    return (
+        <div>
+            <h1 className="text-xl bold">
+                sorry, but we couldnt find that particular snippet
+            </h1>
+        </div>
+    );
+}
